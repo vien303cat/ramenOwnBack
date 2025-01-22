d26d6b1fc8875ad6331e3e7bf3d4d2898532e2dd
VienCat <vien303cat@gmail.com>
2025-01-22 09:03:55 +0800

    first commit

---
+++ controllers/user.js
@@ -0,0 +1,33 @@
+import User from '../models/user.js'
+import validator from 'validator'
+import { StatusCodes } from 'http-status-codes'
+
+export const create = async (req, res) => {
+    try {
+        const user = await User.create(req.body)
+        res.status(StatusCodes.OK).json({
+            success: true,
+            message: '',
+            result: user,
+        })
+    } catch (error) {
+        console.log(error)
+        if (error.name === 'MongoServerError' && error.code === 11000) {
+            res.status(StatusCodes.CONFLICT).json({
+            success: false,
+            message: '帳號重複',
+            })
+        } else if (error.name === 'ValidationError') {
+            const key = Object.keys(error.errors)[0]
+            res.status(StatusCodes.BAD_REQUEST).json({
+                success: false,
+                message: error.errors[key].message,
+            })
+        } else {
+            res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+                success: false,
+                message: '未知錯誤',
+            })
+        }
+    }
+}
